ca0444a813c8727ba4abfa1b23878b8182b19a87
Markus Andresen <markua@stud.ntnu.no>
2015-11-02 13:01:28 +0100

    new javascript menu implemneted

---
+++ webtek-team-graphic/dropdown.js
@@ -0,0 +1,33 @@
+/**
+ * Created by markusandresen on 02/11/15.
+ */
+var timeout	= 20;
+var closetimer	= 0;
+var ddmenuitem	= 0;
+
+function openMenu(id)
+{
+    cancelCloseTime();
+    if(ddmenuitem) ddmenuitem.style.visibility = 'hidden';
+    ddmenuitem = document.getElementById(id);
+    ddmenuitem.style.visibility = 'visible';
+
+}
+
+function close() {
+    if(ddmenuitem) ddmenuitem.style.visibility = 'hidden';
+}
+
+function closeMenu() {
+    closetimer = window.setTimeout(close, timeout);
+}
+
+function cancelCloseTime(){
+    if(closetimer)
+    {
+        window.clearTimeout(closetimer);
+        closetimer = null;
+    }
+}
+
+document.onclick = close();
