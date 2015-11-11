4090a9f5bd5f57f98de19113867ca56c4098d45b
Markus Andresen <markua@stud.ntnu.no>
2015-11-11 19:43:39 +0100

    more phrases

--- webtek-team-graphic/scripts/phrase.js
+++ webtek-team-graphic/scripts/phrase.js
@@ -2,7 +2,8 @@ window.onload = function(){
    var phrases = ["There is no such thing as natural beauty","Luxury salon where you will feel unique and special",
    "The beauty is not luxurious imagination","You are in a beauty contest every day of your life","For The Look of the Future",
    "More defined. More conditioned. More beautiful hair","Satisfying our clients","The beauty is not luxurious imagination","Take time out for beauty",
-   "Beautiful. Colorful. You."];
+   "Beautiful. Colorful. You.","Revitalizing beauty for your hair","The Desired Place For Beauty","Delightful Treats For Your Hair",
+   "Beauty Meets Classy Styles"];
     var random_index = Math.floor(Math.random() * phrases.length);
     console.log(random_index);
     var currentphrase = document.getElementById("phrase");
