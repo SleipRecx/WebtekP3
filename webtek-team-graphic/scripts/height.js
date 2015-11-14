34ab2c8cb6830c1c510c27a46a081bd53c50cf49
Markus Andresen <markua@stud.ntnu.no>
2015-11-14 13:31:50 +0100

    footer responsive

--- webtek-team-graphic/scripts/height.js
+++ webtek-team-graphic/scripts/height.js
@@ -6,7 +6,11 @@ window.addEventListener("load", loaded);
 function loaded(){
     var height = window.innerHeight;
     var wrapper = document.getElementsByClassName("wrapper")[0];
-    console.log(wrapper.style.minHeight);
     wrapper.style.minHeight = height+"px";
     wrapper.style.display = "block";
+
+    window.addEventListener('resize', function(){
+        height = window.innerHeight;
+        wrapper.style.minHeight = height+"px";
+    });
 }
